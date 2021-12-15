3fdddc671cba796f272b0be5bd9b2f19f0115919
PBBBB1969 <83363436+PBBBB1969@users.noreply.github.com>
2021-12-15 14:24:16 -0500

    Add files via upload

---
+++ Game.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+<met charset="utf-8">
+<title>Library Labyrinth</title>
+<body>
+<BODY BACKGROUND= "">
+<FONT FACE="arial,helvetica" SIZE=2><font color="#000000">
+<!------------------------------------------------------------------------------------------------->
+<FONT COLOR="#000000">
+<br>
+<body bgcolor="#ccffe6" marginwidth="0" marginheight="0" leftmargin="0" topmargin="0">
+<center>
+<iframe width="900" height="600" src="game/library labyrinth_pres.ppsx" title="Library Labyrinth" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+
+</center>
+</body>
+</html>
